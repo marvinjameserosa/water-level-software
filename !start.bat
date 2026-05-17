f413aa836dd4264b8ef3833b5f7a6cda246ba487
marvinjameserosa <marvinjameserosa@gmail.com>
2026-05-17 13:21:05 +0800

    feat: add update feat

--- !start.bat
+++ !start.bat
@@ -2,6 +2,9 @@
 echo Installing dependencies...
 call npm i
 
+echo Opening browser to localhost:3000...
+start http://localhost:3000
+
 echo Starting development server...
 npm run dev
 pause
---
+++ !update.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Pulling latest changes from Git...
+git pull
+
+echo.
+echo Update complete!
+pause
